0a655d34011caf5dd1336bfa255a567a1b908cd8
Marcelo Perez <jhkk1408@gmail.com>
2025-09-20 13:54:13 -0300

    two files deleted

--- code 001/code 001.sql
+++
@@ -1,6 +0,0 @@
-SELECT d.department_id, d.department_name, j.job_title
-FROM departments d
-INNER JOIN employees e ON d.department_id = e.department_id
-INNER JOIN jobs j ON e.job_id = j.job_id
-WHERE d.department_id IN (10, 20, 30)
-ORDER BY d.department_id ASC;
